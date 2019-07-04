f20c3c9304b64acaac03328bb5cdd5e7c619def0
Barbara <barbara.pena.a@usach.cl>
2019-07-04 17:16:00 -0400

    admin

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,10 @@ import Router from 'vue-router'
 import Home from '@/components/Home'
 import Voluntariado from '@/components/Voluntariado'
 import Adopciones from '@/components/Adopciones'
+import Adminadopciones from '@/components/Adminadopciones'
+import Adminvoluntariado from '@/components/Adminvoluntariado'
+import Admininicio from '@/components/Admininicio'
+
 
 Vue.use(Router)
 
@@ -22,6 +26,21 @@ export default new Router({
       path: '/adopciones',
       name: 'Adopciones',
       component: Adopciones
+    },
+    {
+      path: '/admin',
+      name: 'Admininicio',
+      component: Admininicio
+    },
+    {
+      path: '/admin/adopciones',
+      name: 'Adminadopciones',
+      component: Adminadopciones
+    },
+    {
+      path: '/admin/voluntariado',
+      name: 'Adminadopciones',
+      component: Adminvoluntariado
     },
   ]
 })
