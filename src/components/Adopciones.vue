ca7d81208f3d2e753be46048b6af4e482fe81be7
Barbara <barbara.pena.a@usach.cl>
2019-07-07 17:43:05 -0400

    detalles de fuente y otros

--- src/components/Adopciones.vue
+++ src/components/Adopciones.vue
@@ -20,8 +20,8 @@
 			</div>
 			
 				<div v-for="perro in perros" class="col-md-3 text-center">
-					<div class="card col-md-12" style="width: 18rem; cursor:pointer;">
-					  <img @click="adoptarEspecifico(perro.id)" :src=perro.url class="card-img-top" alt="...">
+					<div @click="adoptarEspecifico(perro.id)"  class="card col-md-12" style="width: 18rem; cursor:pointer;">
+					  <img :src=perro.url class="card-img-top" alt="...">
 					  <div class="card-body">
 					    <h5 class="card-title text-center"><b>{{perro.nombre}}</b></h5>
 					    <p class="card-text">{{perro.descripcion}}</p>
@@ -42,12 +42,12 @@
 				<!-- Wrapper for carousel items -->
 				<div class="carousel-inner">
 					<div class="item carousel-item active">
-						<div><i class="fas fa-quote-left" style="font-size: 40px;"></i></div>
+						<div><i class="fas fa-quote-left" style="font-size: 40px; color:rgb(40, 133, 135) !important;"></i></div>
 						<p class="testimonial text-center">{{testimonios[0].texto}}</p>
 						<p class="overview"><b>{{testimonios[0].nombre}}</b></p>
 					</div>
 					<div v-for="testimonio in testimonios.slice(1, 4)" class="item carousel-item ">
-						<div><i class="fas fa-quote-left" style="font-size: 40px;"></i></div>
+						<div><i class="fas fa-quote-left" style="font-size: 40px; color:rgb(40, 133, 135) !important;"></i></div>
 						<p class="testimonial text-center">{{testimonio.texto}}</p>
 						<p class="overview"><b>{{testimonio.nombre}}</b></p>
 					</div>
@@ -69,7 +69,7 @@
 
 				
 				<modal v-if="abierto" name="adoptar" :clickToClose="false" :width="800" height="auto" :scrollable="true">
-					 <div class="panel-footer pull-right" style="padding-right: 20px !important; padding-top: 10px !important;">
+					 <div class="panel-footer pull-right text-right" style="padding-right: 20px !important; padding-top: 10px !important;">
 				  	 	   <button v-on:click="cerrar" type="button" class="btn btn-link"><i class="fas fa-times"></i></button>
 				  	 </div>
 				  	<formulario 
@@ -82,9 +82,7 @@
   </div>
 </template>
 <style type="text/css">
-body {
-	font-family: "Open Sans", sans-serif;
-}
+
 h2 {
 	color: #333;
 	text-align: center;
@@ -105,83 +103,15 @@ h2::after {
 	right: 0;
 	bottom: -10px;
 }
-.col-center {
-	margin: 0 auto;
-	float: none !important;
-}
-.carousel {
-	margin: 50px auto;
-	padding: 0 70px;
-}
-.carousel .item {
-	color: #999;
-	font-size: 14px;
-    text-align: center;
-	overflow: hidden;
-    min-height: 290px;
-}
-.carousel .item .img-box {
-	width: 135px;
-	height: 135px;
-	margin: 0 auto;
-	padding: 5px;
-	border: 1px solid #ddd;
-	border-radius: 50%;
-}
-.carousel .img-box img {
-	width: 100%;
-	height: 100%;
-	display: block;
-	border-radius: 50%;
-}
-.carousel .testimonial {
-	padding: 30px 0 10px;
-}
-.carousel .overview {	
-	font-style: italic;
-}
-.carousel .overview b {
-	text-transform: uppercase;
-	color: #7AA641;
-}
-.carousel .carousel-control {
-	width: 40px;
-    height: 40px;
-    margin-top: -20px;
-    top: 50%;
-	background: none;
-}
-.carousel-control i {
-    font-size: 68px;
-	line-height: 42px;
-    position: absolute;
-    display: inline-block;
-	color: rgba(0, 0, 0, 0.8);
-    text-shadow: 0 3px 3px #e6e6e6, 0 0 0 #000;
-}
-.carousel .carousel-indicators {
-	bottom: -40px;
-}
-.carousel-indicators li, .carousel-indicators li.active {
-	width: 10px;
-	height: 10px;
-	margin: 1px 3px;
-	border-radius: 50%;
-}
-.carousel-indicators li {	
-	background: #999;
-	border-color: transparent;
-	box-shadow: inset 0 2px 1px rgba(0,0,0,0.2);
-}
-.carousel-indicators li.active {	
-	background: #555;		
-	box-shadow: inset 0 2px 1px rgba(0,0,0,0.2);
-}
-</style>
-<style>
+
 	.card:hover{
 		background-color:rgba(173, 234, 206,0.3);
 	}
+.testimonial{
+	font-family: 'Arapey', serif !important;
+	font-size: 30px !important;
+	color:rgb(24, 137, 64) !important;
+}
 </style>
 <script>
 	import axios from 'axios';
