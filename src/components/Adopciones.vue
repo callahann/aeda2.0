88ac31e819511e345574ea2a74fae03e6c62f0c4
Barbara <barbara.pena.a@usach.cl>
2019-07-07 17:21:42 -0400

    testimonios y links

--- src/components/Adopciones.vue
+++ src/components/Adopciones.vue
@@ -29,6 +29,43 @@
 					  </div>
 				   </div>
 				</div>
+				<div class="parrafoder col-md-12">
+			<h1 class="col-md-6 text-left"><b>Testimonios</b></h1>
+
+			<div id="myCarousel" class="carousel slide" data-ride="carousel">
+				<!-- Carousel indicators -->
+				<ol class="carousel-indicators">
+					<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+					<li data-target="#myCarousel" data-slide-to="1"></li>
+					<li data-target="#myCarousel" data-slide-to="2"></li>
+				</ol>   
+				<!-- Wrapper for carousel items -->
+				<div class="carousel-inner">
+					<div class="item carousel-item active">
+						<div><i class="fas fa-quote-left" style="font-size: 40px;"></i></div>
+						<p class="testimonial text-center">{{testimonios[0].texto}}</p>
+						<p class="overview"><b>{{testimonios[0].nombre}}</b></p>
+					</div>
+					<div v-for="testimonio in testimonios.slice(1, 4)" class="item carousel-item ">
+						<div><i class="fas fa-quote-left" style="font-size: 40px;"></i></div>
+						<p class="testimonial text-center">{{testimonio.texto}}</p>
+						<p class="overview"><b>{{testimonio.nombre}}</b></p>
+					</div>
+					
+					
+				</div>
+				<!-- Carousel controls -->
+				<a class="carousel-control left carousel-control-prev" href="#myCarousel" data-slide="prev">
+					<i class="fa fa-angle-left"></i>
+				</a>
+				<a class="carousel-control right carousel-control-next" href="#myCarousel" data-slide="next">
+					<i class="fa fa-angle-right"></i>
+				</a>
+			</div>
+		
+
+
+			</div> 
 
 				
 				<modal v-if="abierto" name="adoptar" :clickToClose="false" :width="800" height="auto" :scrollable="true">
@@ -44,6 +81,103 @@
 		</div>
   </div>
 </template>
+<style type="text/css">
+body {
+	font-family: "Open Sans", sans-serif;
+}
+h2 {
+	color: #333;
+	text-align: center;
+	text-transform: uppercase;
+	font-family: "Roboto", sans-serif;
+	font-weight: bold;
+	position: relative;
+	margin: 30px 0 60px;
+}
+h2::after {
+	content: "";
+	width: 100px;
+	position: absolute;
+	margin: 0 auto;
+	height: 3px;
+	background: #8fbc54;
+	left: 0;
+	right: 0;
+	bottom: -10px;
+}
+.col-center {
+	margin: 0 auto;
+	float: none !important;
+}
+.carousel {
+	margin: 50px auto;
+	padding: 0 70px;
+}
+.carousel .item {
+	color: #999;
+	font-size: 14px;
+    text-align: center;
+	overflow: hidden;
+    min-height: 290px;
+}
+.carousel .item .img-box {
+	width: 135px;
+	height: 135px;
+	margin: 0 auto;
+	padding: 5px;
+	border: 1px solid #ddd;
+	border-radius: 50%;
+}
+.carousel .img-box img {
+	width: 100%;
+	height: 100%;
+	display: block;
+	border-radius: 50%;
+}
+.carousel .testimonial {
+	padding: 30px 0 10px;
+}
+.carousel .overview {	
+	font-style: italic;
+}
+.carousel .overview b {
+	text-transform: uppercase;
+	color: #7AA641;
+}
+.carousel .carousel-control {
+	width: 40px;
+    height: 40px;
+    margin-top: -20px;
+    top: 50%;
+	background: none;
+}
+.carousel-control i {
+    font-size: 68px;
+	line-height: 42px;
+    position: absolute;
+    display: inline-block;
+	color: rgba(0, 0, 0, 0.8);
+    text-shadow: 0 3px 3px #e6e6e6, 0 0 0 #000;
+}
+.carousel .carousel-indicators {
+	bottom: -40px;
+}
+.carousel-indicators li, .carousel-indicators li.active {
+	width: 10px;
+	height: 10px;
+	margin: 1px 3px;
+	border-radius: 50%;
+}
+.carousel-indicators li {	
+	background: #999;
+	border-color: transparent;
+	box-shadow: inset 0 2px 1px rgba(0,0,0,0.2);
+}
+.carousel-indicators li.active {	
+	background: #555;		
+	box-shadow: inset 0 2px 1px rgba(0,0,0,0.2);
+}
+</style>
 <style>
 	.card:hover{
 		background-color:rgba(173, 234, 206,0.3);
@@ -58,13 +192,18 @@
 	      	perros: [],
 	      	abierto: false,
 	      	tipo_adopcion: '',
-	      	perroSeleccionado: 0
+	      	perroSeleccionado: 0,
+	      	testimonios:[]
 	      }
 	    },
 	    created: function(){
 	    	axios.get('http://localhost:3000/mascotas')
             .then(response => {
                 this.perros = response.data;     
+            });
+            axios.get('http://localhost:3000/testimoniosadopcion')
+            .then(response => {
+                this.testimonios = response.data;     
             });
 	    },
 	    methods:{
--- src/components/Voluntariado.vue
+++ src/components/Voluntariado.vue
@@ -26,26 +26,84 @@
 				
 
 			</div>
-			<!-- <div class="parrafoder col-md-12">
-				<h1 class="col-md-6 text-left"><b>Testimonios</b></h1>
-				<div>
+			<div class="parrafoder col-md-12">
+			<h1 class="col-md-6 text-left"><b>Testimonios</b></h1>
+
+			<div id="myCarousel" class="carousel slide" data-ride="carousel">
+				<!-- Carousel indicators -->
+				<ol class="carousel-indicators">
+					<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+					<li data-target="#myCarousel" data-slide-to="1"></li>
+					<li data-target="#myCarousel" data-slide-to="2"></li>
+				</ol>   
+				<!-- Wrapper for carousel items -->
+				<div class="carousel-inner">
+					<div class="item carousel-item active">
+						<div><i class="fas fa-quote-left" style="font-size: 40px;"></i></div>
+						<p class="testimonial text-center">{{testimonios[0].texto}}</p>
+						<p class="overview"><b>{{testimonios[0].nombre}}</b></p>
+					</div>
+					<div v-for="testimonio in testimonios.slice(1, 4)" class="item carousel-item ">
+						<div><i class="fas fa-quote-left" style="font-size: 40px;"></i></div>
+						<p class="testimonial text-center">{{testimonio.texto}}</p>
+						<p class="overview"><b>{{testimonio.nombre}}</b></p>
+					</div>
+					
 					
 				</div>
-				
-				
-				
+				<!-- Carousel controls -->
+				<a class="carousel-control left carousel-control-prev" href="#myCarousel" data-slide="prev">
+					<i class="fa fa-angle-left"></i>
+				</a>
+				<a class="carousel-control right carousel-control-next" href="#myCarousel" data-slide="next">
+					<i class="fa fa-angle-right"></i>
+				</a>
+			</div>
+		
 
-			</div> -->
+
+			</div> 
 
 
 		</div>
   </div>
 </template>
+<style type="text/css">
+h2 {
+	color: #333;
+	text-align: center;
+	text-transform: uppercase;
+	font-family: "Roboto", sans-serif;
+	font-weight: bold;
+	position: relative;
+	margin: 30px 0 60px;
+}
+h2::after {
+	content: "";
+	width: 100px;
+	position: absolute;
+	margin: 0 auto;
+	height: 3px;
+	background: #8fbc54;
+	left: 0;
+	right: 0;
+	bottom: -10px;
+}
+
+</style>
 <script>
+  import axios from 'axios';
   export default{
     data(){
       return{
+      	testimonios:[]
       }
-    }
+    },
+     created: function(){
+            axios.get('http://localhost:3000/testimoniosvoluntariado')
+            .then(response => {
+                this.testimonios = response.data;     
+            });
+	    },
   }
 </script>
